560ff96b0470e94b2cf6dd416db0721a92a03c63
driwnet <63108525+driwnet@users.noreply.github.com>
2020-12-10 17:37:07 +0100

    Update exercise_3.c

--- assignment_4/exercise_3/exercise_3.c
+++ assignment_4/exercise_3/exercise_3.c
@@ -7,7 +7,9 @@
 #include <sys/time.h>
 
 
-#define ARRAY_SIZE 100000
+#define NUM_PARTICLES 10000
+#define NUM_ITERATIONS 1000
+#define BLOCK_SIZE 64
 #define error 1e-6
 // This is a macro for checking the error variable:>.
 #define CHK_ERROR(err) if (err != CL_SUCCESS) fprintf(stderr,"Error: %s\n",clGetErrorString(err));
@@ -16,19 +18,29 @@
 const char* clGetErrorString(int);
 
 
+
+
 const char *mykernel = 
 "__kernel                                                 \n" 
-"void kernel_saxpy (                                      \n"
-"   const int n,                                          \n"
-"   const float a,                                        \n"    
-"   __global float* X,                                    \n"   
-"   __global float* Y)                                    \n"
-"{                                                        \n"
-"   int index = get_global_id(0);                         \n"
-"   if(index < n)                                         \n"
-"   Y[index] = a * X[index] + Y[index];                   \n"
+"void kernel_upload(                                       \n"
+"   const int iter,                                          \n"
+"   const int nump,                                        \n"    
+"   __global particle* X)                                    \n"                                      
+"{int index = get_global_id(0);                           \n"
+"if(index < nump) {                                                \n"
+"   X[index].velocity[0] = (3*index + iter) % nump;             \n"
+"   X[index].velocity[1] = (4*index + iter) % nump;            \n"
+"   X[index].velocity[2] = (5*index + iter) % nump;             \n"
+"   X[index].position[0] = X[index].position[0] + X[index].velocity[0];             \n"    
+"   X[index].position[1] = X[index].position[1] + X[index].velocity[1];             \n"
+"   X[index].position[2] = X[index].position[2] + X[index].velocity[2];             \n"
+"}                                                          \n"
 "}                                                        \n";
 
+struct __atribute__ ((packed)) particle{
+    float position[3];
+    float velocity[3];
+};
 
 double cpuSecond() {
     struct timeval tp;
@@ -36,18 +48,28 @@ double cpuSecond() {
     return ((double)tp.tv_sec + (double)tp.tv_usec*1.e-6);
 }
 
+void init_Array(particle *particulas){
+    for(int i = 0; i < NUM_PARTICLES; i++){
+        particulas[i].position[0] = rand() % 1000;
+        particulas[i].position[1] = rand() % 1000;
+        particulas[i].position[2] = rand() % 1000;
+
+        particulas[i].velocity[0] = rand() % 1000;
+        particulas[i].velocity[1] = rand() % 1000;
+        particulas[i].velocity[2] = rand() % 1000;
+    }
+}
+
 
 int main(int argc, char **argv) {
 
     //All the variables used
-    float X[ARRAY_SIZE], Y[ARRAY_SIZE], results[ARRAY_SIZE];
-    float array = ARRAY_SIZE * sizeof(float);
-    float a;
-    int count = ARRAY_SIZE;
+    particle X[NUM_PARTICLES],  results[NUM_PARTICLES];
+    float array = NUM_PARTICLES * sizeof(particle);
+    int count = NUM_PARTICLES;
     bool correct = true;
-    int i;
+    int i, j;
 
-    a = 3.0;
     cl_platform_id * platforms; cl_uint     n_platform;
 
     // Find OpenCL Platforms
@@ -70,44 +92,48 @@ int main(int argc, char **argv) {
     }
 
     // Create a command queue
-    cl_command_queue cmd_queue = clCreateCommandQueue(context, device_list[0], 0, &err);CHK_ERROR(err); 
+    cl_command_queue cmd_queue = clCreateCommandQueue(context, device_list[0], 0, &err);CHK_ERROR(err);
 
     if (!cmd_queue) {
         printf("Error: Failed to create a command queue!\n");
         return EXIT_FAILURE;
     }
 
+
     /* Insert your own code here */
 
     //program
     cl_program program = clCreateProgramWithSource(context, 1, (const char **)&mykernel, NULL, &err);CHK_ERROR(err);
 
+
     //Initialize values of the arrays
  
-    for(i = 0; i < ARRAY_SIZE; i++) {
-        X[i] = rand() / (float)RAND_MAX;
-        Y[i] = rand() / (float)RAND_MAX;
-    }
-
-    //Cpu part
-    double start = cpuSecond();
-    for(i = 0; i < ARRAY_SIZE; i++) {
-        results[i] = a * X[i] + Y[i];
-    }
-    double totalCpu = cpuSecond() - start;        
-
-    printf("Computing SAXPY on the CPU... Done\n");
+    init_Array(X);
 
     //Array on GPU part
-    cl_mem dx = clCreateBuffer(context, CL_MEM_READ_ONLY, array, NULL, &err);CHK_ERROR(err);
-    cl_mem dy = clCreateBuffer(context, CL_MEM_READ_WRITE, array, NULL, &err);CHK_ERROR(err);
+    cl_mem dx = clCreateBuffer(context, CL_MEM_READ_WRITE, array, NULL, &err);CHK_ERROR(err);
 
 
     //Copy the values of X in dx
     err = clEnqueueWriteBuffer(cmd_queue, dx, CL_TRUE, 0, array, X, 0, NULL, NULL);CHK_ERROR(err);
-    err = clEnqueueWriteBuffer(cmd_queue, dy, CL_TRUE, 0, array, Y, 0, NULL, NULL);CHK_ERROR(err);
 
 
+    //Cpu part
+    double start = cpuSecond();
+    for(j = 0; j < NUM_ITERATIONS; j++) {
+        for(i = 0; i < NUM_PARTICLES; i++){
+            X[i].velocity[0] = (3*i + j) % NUM_PARTICLES;             
+            X[i].velocity[1] = (4*i + j) % NUM_PARTICLES;            
+            X[i].velocity[2] = (5*i + j) % NUM_PARTICLES;             
+            X[i].position[0] = X[i].position[0] + X[i].velocity[0];                 
+            X[i].position[1] = X[i].position[1] + X[i].velocity[1];            
+            X[i].position[2] = X[i].position[2] + X[i].velocity[2]; 
+        }
+    }
+    double totalCpu = cpuSecond() - start;        
+
+    printf("Computing SAXPY on the CPU... Done\n");
+    
     //build the program 
     err = clBuildProgram(program, 1, device_list, NULL, NULL, NULL);
     if (err != CL_SUCCESS){
@@ -119,43 +145,48 @@ int main(int argc, char **argv) {
     }
 
     //Create the kernel function
-    cl_kernel kernel = clCreateKernel(program, "kernel_saxpy", &err);CHK_ERROR(err);
+    cl_kernel kernel = clCreateKernel(program, "kernel_upload", &err);CHK_ERROR(err);
     
 
-    //Arguments for the Kernel function
-    err = clSetKernelArg(kernel, 0, sizeof(int), (void*) &count);CHK_ERROR(err);
-    err = clSetKernelArg(kernel, 1, sizeof(float), (void*) &a);CHK_ERROR(err);
-    err = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void*) &dx);CHK_ERROR(err);
-    err = clSetKernelArg(kernel, 3, sizeof(cl_mem), (void*) &dy);CHK_ERROR(err);
 
     //More argument, number of workitems we are going to use
-    unsigned int mult = 256;
-    while(mult < ARRAY_SIZE){
+    unsigned int mult = BLOCK_SIZE;
+    unsigned int size = BLOCK_SIZE;
+    while(mult < NUM_PARTICLES){
 
         mult = mult * 2;
     }
     size_t n_workitem[1] = {mult};
-    size_t workgroup_size[1] = {256};
+    size_t workgroup_size[1] = {size};
+
 
 
     double st = cpuSecond();
-    //Launch kernel
-    err = clEnqueueNDRangeKernel(cmd_queue, kernel, 1, NULL, n_workitem, workgroup_size, 0, NULL, NULL);CHK_ERROR(err);
+    for(i = 0; i < NUM_PARTICLES; i++){
 
-    err = clEnqueueReadBuffer(cmd_queue, dy, CL_TRUE, 0, array, Y, 0, NULL, NULL);CHK_ERROR(err);
+        //Arguments for the Kernel function
+        err = clSetKernelArg(kernel, 0, sizeof(int), (void*) &i);CHK_ERROR(err);
+        err = clSetKernelArg(kernel, 1, sizeof(int), (void*) &count);CHK_ERROR(err);
+        err = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void*) &dx);CHK_ERROR(err);
+
+        //Launch kernel
+        err = clEnqueueNDRangeKernel(cmd_queue, kernel, 1, NULL, n_workitem, workgroup_size, 0, NULL, NULL);CHK_ERROR(err);
+
+
+        //Wait for everything to finish
+        err = clFlush(cmd_queue);
+        err = clFinish(cmd_queue);
+    }
+    err = clEnqueueReadBuffer(cmd_queue, dx, CL_TRUE, 0, array, results, 0, NULL, NULL);CHK_ERROR(err);
     double timeGpu = cpuSecond() - st;
     printf("Computing SACY on the GPU... Done\n");
 
-    //Wait for everything to finish
-    err = clFlush(cmd_queue);
-    err = clFinish(cmd_queue);
-    
-
-
-    for(i = 0; i < ARRAY_SIZE; i++){
-        if (fabs(results[i] - Y[i]) > error){
-            correct = false;
-            break;
+    for(int i = 0; i < NUM_PARTICLES; i++){
+        for(int dim = 0; dim < 3; dim++){
+            if(abs(results[i].position[dim] - X[i].position[dim]) > error ){
+                correct = false;
+                break;
+            }
         }
     }
 
@@ -166,14 +197,15 @@ int main(int argc, char **argv) {
         printf("Comparing the output for each implementation... Incorrect!\n");
     }
 
-    printf("ARRAY_SIZE: %d\n", ARRAY_SIZE);
+    printf("NUM_PARTICLES: %d\n", NUM_PARTICLES);
+    printf("BLOCK_SIZE: %d\n", BLOCK_SIZE);
+    printf("NUM_ITERATIONS: %d\n", NUM_ITERATIONS);
     printf("Execution time on CPU: %f\n", totalCpu);
     printf("Execution time on GPU: %f\n", timeGpu);
 
 
     // Finally, release all that we have allocated.
     clReleaseMemObject(dx);CHK_ERROR(err);
-    clReleaseMemObject(dy);CHK_ERROR(err);
     clReleaseProgram(program);CHK_ERROR(err);
     clReleaseKernel(kernel);CHK_ERROR(err);
     err = clReleaseCommandQueue(cmd_queue);CHK_ERROR(err);
@@ -281,3 +313,4 @@ const char* clGetErrorString(int errorCode) {
     default: return "CL_UNKNOWN_ERROR";
     }
 }
+
